730f54f81da0813af4953bf54865f3804d598f5f
Facundo Parra <facundo.parra.94@gmail.com>
2018-12-09 19:27:28 -0300

    Day 6 part1 improvement

--- 6/day6.py
+++ 6/day6.py
@@ -11,41 +11,41 @@ def print_grid(grid, bool_print=False):
 
 # Creating grids
 grid_side_size = 400
-distance_grids = {}
+distance_grid = [[{'areas':[], 'distance':grid_side_size*3} for _ in range(grid_side_size)] for i in range(grid_side_size)]
 area_size = {}
+area_centers = {}
 
-# Completing grids with input
 with open('input.txt','r') as input:
   area_id = 0
   for pair in input:
     y, x = map(lambda x: int(x), pair[:-1].split(', '))
     area_id += 1
-    distance_grids[area_id] = [[0]*grid_side_size for i in range(grid_side_size)]
+    area_centers[area_id] = (x, y)
     area_size[area_id] = 0
 
-    for row in range(grid_side_size):
-      for column in range(grid_side_size):
-        distance = abs(row-y) + abs(column-x)
-        distance_grids[area_id][row][column] = distance
+for x in range(grid_side_size):
+  for y in range(grid_side_size):
+    
+    for area_id, area_center in area_centers.items():
+      distance = abs(x-area_center[0]) + abs(y-area_center[1])
+      
+      if distance < distance_grid[y][x]['distance']:
+        distance_grid[y][x]['areas'] = [area_id]
+        distance_grid[y][x]['distance'] = distance
 
+      elif distance == distance_grid[y][x]['distance']:
+        distance_grid[y][x]['areas'].append(area_id)
+    
 for row in range(grid_side_size):
   for column in range(grid_side_size):
-    closest_area = 0
-    closest_area_distance = grid_side_size*2+1
-    two_same_distance = False
-    for area_id, distance_grid in distance_grids.items():
-      if distance_grid[row][column] < closest_area_distance:
-        closest_area_distance = distance_grid[row][column]
-        closest_area = area_id
-        two_same_distance = False
-      elif distance_grid[row][column] == closest_area_distance:
-        two_same_distance = True
     
-    if row == 0 or column==0 or row==grid_side_size-1 or column==grid_side_size-1:
-      if closest_area in area_size:
-        area_size.pop(closest_area)
+    if len(distance_grid[row][column]['areas']) == 1:
 
-    if not two_same_distance and closest_area in area_size:
-      area_size[closest_area] += 1
+      if row==0 or column==0 or row==grid_side_size-1 or column==grid_side_size-1:
+        if distance_grid[row][column]['areas'][0] in area_size:
+          area_size.pop(distance_grid[row][column]['areas'][0])
+      
+      elif distance_grid[row][column]['areas'][0] in area_size:
+        area_size[distance_grid[row][column]['areas'][0]] += 1
 
 print(max(area_size.values()))
