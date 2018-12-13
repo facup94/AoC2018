8868e51f545163978fc6ad3229c14bef3340f1ee
Facundo Parra <facundo.parra@claro.com.ar>
2018-12-13 08:53:21 -0300

    Day 11 - Summed Area Table

--- 11/day11b.py
+++ 11/day11b.py
@@ -12,23 +12,13 @@ for y in range(300):
     power_level -= 5
     grid[y][x] = power_level
 
-for row in grid[:10]:
-  print(row[:10])
-
 pre_processed_grid = [[0]*300 for _ in range(300)]
 for y_original_grid in range(300):
-  print('\rFilling pre-processed grid - ROW:', y_original_grid, '/ 300', end='')
   for x_original_grid in range(300):
     for y in range(y_original_grid, 300):
       for x in range(x_original_grid, 300):
         pre_processed_grid[y][x] += grid[y_original_grid][x_original_grid]
 
-print('')
-
-for row in pre_processed_grid[:10]:
-  print(row[:10])
-
-print('cortar')
 
 largest_power_sum = 0
 largest_power_sum_x = 0
@@ -36,15 +26,25 @@ largest_power_sum_y = 0
 largest_power_sum_size = 0
 
 for side_size in range(1, 301):
-  print('Testing size ', side_size, ' (', str(side_size*100/300)[:4], '%)', sep='')
+  # print('Testing size ', side_size, ' (', str(side_size*100/300)[:4], '%)', sep='')
   for y_start in range(301 - side_size):
     for x_start in range(301 - side_size):
-      power_sum = sum([grid[y][x] for y in range(y_start, y_start + side_size) for x in range(x_start, x_start + side_size)])
+      power_sum = pre_processed_grid[y_start+side_size-1][x_start+side_size-1]
       
+      if y_start-1 >= 0 and x_start+side_size-1 >= 0:
+        power_sum -= pre_processed_grid[y_start-1][x_start+side_size-1]
+      
+      if y_start+side_size-1 >= 0 and x_start-1 >= 0:
+        power_sum -= pre_processed_grid[y_start+side_size-1][x_start-1]
+      
+      if y_start-1 >= 0 and x_start-1 >= 0:
+        power_sum += pre_processed_grid[y_start-1][x_start-1]
+
+
       if power_sum > largest_power_sum:
         largest_power_sum = power_sum
-        largest_power_sum_x = x
-        largest_power_sum_y = y
+        largest_power_sum_x = x_start
+        largest_power_sum_y = y_start
         largest_power_sum_size = side_size
 
-print(largest_power_sum_x, largest_power_sum_y, side_size, sep=',')
+print(largest_power_sum_x, largest_power_sum_y, largest_power_sum_size, sep=',')
